b12f701ec07a55a16baf99f0953e3c27a62a8507
AleMarinAsc <ale.m.ascencio@hotmail.com>
2021-09-24 16:23:20 -0500

    Agregar archivos de proyecto.

---
+++ App3/App3/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        private string _nombre;
+        private string _apellidoPaterno;
+        private string _apellidoMaterno;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.Equals(_nombre, value)) return;
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string ApellidoPaterno
+        {
+            get => _apellidoPaterno;
+            set
+            {
+                if (string.Equals(_apellidoPaterno, value)) return;
+                _apellidoPaterno = value;
+                RaisePropertyChanged(nameof(ApellidoPaterno));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string ApellidoMaterno
+        {
+            get => _apellidoMaterno;
+            set
+            {
+                if (string.Equals(_apellidoMaterno, value)) return;
+                _apellidoMaterno = value;
+                RaisePropertyChanged(nameof(ApellidoMaterno));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string NombreCompleto
+        {
+            get => $"Nombre completo: {Nombre} {ApellidoPaterno} {ApellidoMaterno}";
+        }
+
+    }
+}
---
+++ App3/App3/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void RaisePropertyChanged(string nombrePropierdad)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nombrePropierdad));
+        }
+
+
+    }
+}
